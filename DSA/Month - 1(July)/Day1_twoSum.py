e676e27d59f1c1db191343c32e8fb8c9e695d311
Sujata Bhadake <sujatabhadke0@gmail.com>
2026-07-01 12:17:46 +0530

    Day 2 - Two Sum brute force solution with complexity analysis

--- DSA/Month - 1(July)/Day1_twoSum.py
+++ DSA/Month - 1(July)/Day1_twoSum.py
@@ -1,5 +1,20 @@
+'''
+Approach
+Iterate through every element of the array.
+For each element, compute the value required to reach the target.
+Search the entire array to find that required value.
+Ensure that the same index is not used twice.
+As soon as a valid pair is found, return both indices because the problem guarantees exactly one solution.
+
+'''
+
+## The solution is not the best or optimized efficient I know, But I learnt a lot of things while solving this problem and I am proud that I found out the solution on my own after getting so many errors, after my code failing so many times, this is the brute force approach I discovered and in future as I learn more things, I will optimize my solution
+
+nums = [2,7,3,4]
+target = 5
+
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, nums: list[int], target: int) -> list[int]:
         indices = []
         
         for index, val in enumerate(nums):
@@ -9,6 +24,41 @@ class Solution:
                     if num==s:
                         if i!=index:
                             indices.append(index)
+                            indices.append(i)
+                            return indices
+
+solution = Solution()
+
+print(solution.twoSum(nums, target))       
+    
+
+
+'''
+Algorithm (Brute Force)
+Create an empty list indices.
+Traverse the list using enumerate() to get the current index and value.
+
+For each element, calculate its complement:
+
+complement = target - current_value
+Traverse the list again to search for the complement.
+If the complement is found and its index is different from the current index:
+Store both indices.
+Return the list of indices immediately.
+If no valid pair is found, the function ends (though in the Two Sum problem, it is guaranteed that one valid pair exists).
+
+
+'''
+
+'''
+Time Complexity
+Outer loop: O(n)
+Inner loop: O(n)
+Overall: O(n²)
+Space complexity: O(2)
+'''
+
+                        
 
                 
                 
@@ -18,6 +68,17 @@ class Solution:
 
                     
 
+                
+                
+                    
+
+                
+                
+        
+            
+        
+        
+        
                 
                 
                     
