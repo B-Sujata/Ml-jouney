ac158d9315b7183c7c85ec5858c722872acec1f8
Sujata Bhadake <sujatabhadke0@gmail.com>
2026-07-26 22:21:39 +0530

    Day 18 #509 --> Fibonacci Sequence first attempt using Recursion

---
+++ DSA/Month - 1(July)/Day18_507_Fibonacci.py
@@ -0,0 +1,33 @@
+# Guess What I Solved this problem all on my own in just 10 mins, First time solving recursion, I'm so happieee
+
+class Solution:
+    def fib(self, n: int) -> int:
+        
+        if n==0:
+            return 0
+        elif n == 1:
+            return 1
+        return self.fib(n-1)+self.fib(n-2)
+
+
+'''
+Overall Analysis
+
+Approach: Recursive (Top-Down without memoization)
+
+Algorithm:
+
+If n == 0, return 0.
+If n == 1, return 1.
+Otherwise, recursively compute fib(n-1) and fib(n-2).
+Return their sum.
+
+Time Complexity: O(2^n)
+
+Space Complexity: O(n)
+
+
+'''
+
+            
+        
